cca844179b5492884e4895bbff449221c7ab8818
Marina Iglesia Pérez <iglesiaperezmarina@gmail.com>
2022-10-24 14:12:17 +0200

    primera subida

---
+++ funciones.py
@@ -0,0 +1,10 @@
+def DevuelveMax (param1, param2):
+    if param1 > param2:
+        print(param1)
+    elif param2 > param1:
+        print(param2)    
+    else:
+        print("Los dos números son iguales") 
+
+
+
---
+++ main.py
@@ -0,0 +1,18 @@
+from funciones import DevuelveMax
+
+num1=int(input("Introduzca el primer número: "))
+num2=int(input("Introduzca el segundo número: "))
+   
+
+print("El número más alto es: ")        
+
+DevuelveMax (num1, num2)    
+
+numero1=int(input("Introduzca el primer número: "))
+numero2=int(input("Introduzca el segundo número: "))
+
+  
+
+print("El número más alto es: ")        
+
+DevuelveMax (numero1, numero2)    
